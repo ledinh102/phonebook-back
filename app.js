bf46d55a70e966b182f1266dcbe9351d441a9df4
kay <lenguyennhutdinh>
2023-04-28 09:26:36 +0700

    add routes

--- app.js
+++ app.js
@@ -1,56 +1,43 @@
 const express = require("express")
+const cors = require("cors")
 const morgan = require("morgan")
 
-const app = express()
+const Person = require("./mongo")
 
-morgan.token('req-body', (req) => JSON.stringify(req.body));
+morgan.token("req-body", (req) => JSON.stringify(req.body))
+
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 // use Morgan middleware with the custom format string
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
-
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-]
+app.use(
+	morgan(
+		":method :url :status :res[content-length] - :response-time ms :req-body"
+	)
+)
 
 app.get("/api/persons", (req, res) => {
-	res.json(persons)
+	Person.find({}).then((persons) => {
+		res.json(persons)
+	})
 })
 
 app.post("/api/persons", (req, res) => {
-	const person = {
-		id: Math.floor(Math.random() * 100),
-		name: "John Smith",
-		number: "94238 782",
-	}
-	res.status(200).json({ success: true });
-	console.log(person);
-	persons.push(person)
-	// console.log(persons)
+	const newPerson = new Person(req.body)
+	console.log(newPerson)
+	newPerson.save().then(() => {
+		Person.find({}).then((persons) => {
+			res.json(persons)
+		})
+	})
 })
 
-app.get("/api/persons/:id", (req, res) => {
-	const id = Number(req.params.id)
-	const person = persons.find((person) => person.id === id)
-
+app.get("/api/persons/:id", async (req, res) => {
+	const id = req.params.id
+	const person = await Person.findOne({_id: id}).then((returnedPerson) => {
+		return returnedPerson
+	})
 	if (person) {
 		res.json(person)
 	} else {
@@ -58,20 +45,19 @@ app.get("/api/persons/:id", (req, res) => {
 	}
 })
 
-app.delete("/api/persons/:id", (req, res) => {
-	const id = Number(req.params.id)
-	persons = persons.filter((person) => person.id !== id)
-
+app.delete("/api/persons/:id", async (req, res) => {
+	const id = req.params.id
+	await Person.deleteOne({_id: id})
 	res.status(204).end()
 })
 
-app.post("/info", (req, res) => {
-	const person = req.body
-	console.log(person)
-	res.send(
-		`<p>Phonebook has info for ${person.length}</p><p>${new Date()}</p>`
-	)
-})
+// app.post("/info", (req, res) => {
+// 	const person = req.body
+// 	console.log(person)
+// 	res.send(
+// 		`<p>Phonebook has info for ${person.length}</p><p>${new Date()}</p>`
+// 	)
+// })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ mongo.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const uri = process.env.MONGODB_URI
+
+mongoose.set("strictQuery", false)
+
+mongoose.connect(uri).then(() => {
+	console.log("Connected to Mongo")
+})
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	phone: String,
+})
+
+personSchema.set("toJSON", {
+	transform: (document, returnedObject) => {
+		delete returnedObject.__v
+	},
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+module.exports = Person
